47315f78a8de04938c7d03228fb13033eba642b4
Chris Weber <chris.weber@rethinc.ch>
2025-08-19 21:42:06 +0200

    Dockerize web service

--- web_service/src/main.rs
+++ web_service/src/main.rs
@@ -98,7 +98,7 @@ async fn main() -> Result<(), std::io::Error> {
         .server("http://localhost:3000/api");
     let ui = api_service.swagger_ui();
     let app = Route::new().nest("/", ui).nest("/api", api_service);
-    Server::new(TcpListener::bind("127.0.0.1:3000"))
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
 }
