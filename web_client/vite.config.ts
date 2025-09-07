e9a7a97ef6ed76d983e52a6b0eefc7228bd89d70
Chris Weber <chris.weber@rethinc.ch>
2025-09-07 20:59:22 +0200

    Configure proxy for local api requests in the dev environment

--- web_client/vite.config.ts
+++ web_client/vite.config.ts
@@ -7,6 +7,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
