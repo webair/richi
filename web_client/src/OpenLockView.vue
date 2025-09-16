5dfda583bc5390e0441c9a078393fc70036831db
Chris Weber <chris.weber@rethinc.ch>
2025-09-16 10:34:47 +0200

    Reset open lock state after success

--- web_client/src/OpenLockView.vue
+++ web_client/src/OpenLockView.vue
@@ -1,19 +1,22 @@
 <script lang="ts" setup>
 import type { Session } from '@supabase/supabase-js'
+import { watch } from 'vue'
 
 import { useAsyncState } from './shared/useAsyncState'
 import { authClient, withStandardizedError } from './supabase'
 
 const props = defineProps<{ session: Session }>()
 
-const { state: requestOpenLockState, execute: requestOpenLock } = useAsyncState(
-  async (accessToken: string) => {
-    await fetch('api/open-lock', {
-      method: 'POST',
-      headers: { Authorization: `Bearer ${accessToken}` },
-    })
-  },
-)
+const {
+  state: lockRequestState,
+  execute: requestOpenLock,
+  reset: resetLockRequest,
+} = useAsyncState(async (accessToken: string) => {
+  await fetch('api/open-lock', {
+    method: 'POST',
+    headers: { Authorization: `Bearer ${accessToken}` },
+  })
+})
 
 const onLogout = async () => {
   await withStandardizedError(() => authClient.signOut())
@@ -22,6 +25,14 @@ const onLogout = async () => {
 const onRequestOpenLock = async () => {
   requestOpenLock(props.session.access_token)
 }
+
+watch(lockRequestState, () => {
+  if (lockRequestState.value.type === 'success-state') {
+    setTimeout(() => {
+      resetLockRequest()
+    }, 5000)
+  }
+})
 </script>
 
 <template>
@@ -29,15 +40,17 @@ const onRequestOpenLock = async () => {
     <input type="submit" value="Abmelden" />
   </form>
 
-  <form v-if="requestOpenLockState.type === 'idle-state'" @submit.prevent="onRequestOpenLock">
+  <form
+    v-if="lockRequestState.type === 'idle-state' || lockRequestState.type === 'error-state'"
+    @submit.prevent="onRequestOpenLock"
+  >
+    <p v-if="lockRequestState.type === 'error-state'">
+      Ups da ist etwas schief gelaufen: {{ lockRequestState.error.message }}
+    </p>
     <input type="submit" value="Schloss öffnen" />
   </form>
 
-  <p v-if="requestOpenLockState.type === 'processing-state'">Anfrage zum Öffnen wird gesendet...</p>
+  <p v-if="lockRequestState.type === 'processing-state'">Anfrage zum Öffnen wird gesendet...</p>
 
-  <p v-if="requestOpenLockState.type === 'success-state'">Schloss wird geöffnet.</p>
-
-  <p v-if="requestOpenLockState.type === 'error-state'">
-    Ups da ist etwas schief gelaufen: {{ requestOpenLockState.error.message }}
-  </p>
+  <p v-if="lockRequestState.type === 'success-state'">Schloss wird geöffnet.</p>
 </template>
